f25111697b021789892e8239f9df589b21be4ad3
Pat Ugarte <patriciomugarte@gmail.com>
2021-05-15 23:47:27 -0300

    Incoporada la sección de exchange y mejoras en los estilos

--- js/index.js
+++ js/index.js
@@ -1,14 +1,37 @@
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
-);
+const toggleSwitch = document.querySelector("#theme-switch-input");
 
 document.documentElement.setAttribute("app-theme", "light");
 toggleSwitch.checked = false;
 
 const switchTheme = (e) => {
-  e.target.checked
-    ? document.documentElement.setAttribute("app-theme", "dark")
-    : document.documentElement.setAttribute("app-theme", "light");
+  const theme = e.target.checked ? "dark" : "light";
+  document.documentElement.setAttribute("app-theme", theme);
 };
 
 toggleSwitch.addEventListener("change", switchTheme, false);
+
+const leftArrow = document.querySelector("#table-left-arrow");
+const rightArrow = document.querySelector("#table-right-arrow");
+
+const tableData = [
+  {
+    type: "Monedas",
+    data: [
+      { name: "Bitcoin", value: 1.96, state: "down" },
+      { name: "Ethereum", value: 0.07, state: "up" },
+      { name: "Ripple", value: 2.15, state: "down" },
+      { name: "Stellar", value: 4.96, state: "down" },
+    ],
+    updatedAt: "19 Julio 23:45",
+  },
+  {
+    type: "Comisiones",
+    data: [
+      { name: "Bitrade", value: 12.96 },
+      { name: "Bitpreco", value: 13.07 },
+      { name: "Novadax", value: 13.15 },
+      { name: "Coinext", value: 14.96 },
+    ],
+    updatedAt: "19 Julio 23:48",
+  },
+];
